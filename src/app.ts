591c353f746b520ee35e59754307788afaa070e0
Sharif-Billah <sharifbillah1998@gmail.com>
2023-06-05 22:30:14 +0600

    [DONE] improve lint setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersService from './app/modules/users/users.service'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -8,7 +10,17 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// application routes
+app.use('/api/v1/users/', usersRouter)
+
+// testing
+app.get('/', async (req: Request, res: Response) => {
+  await usersService.createUser({
+    id: '9999',
+    password: '1234',
+    role: 'student',
+  })
+
   res.send('Working successfully')
 })
 
