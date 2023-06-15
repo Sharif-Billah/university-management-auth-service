80fe5b6077fa07d17ad8fc5f116a6a3e074cc104
Sharif-Billah <sharifbillah1998@gmail.com>
2023-06-15 09:31:00 +0600

    [DONE] student create

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,46 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-import usersService from './app/modules/users/users.service'
-import usersRouter from './app/modules/users/users.route'
-const app: Application = express()
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import router from './app/routes';
+import { StatusCodes } from 'http-status-codes';
+// import { generateFacultyId } from './app/modules/user/user.utils';
+const app: Application = express();
 
-app.use(cors())
+app.use(cors());
 
 // parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// application routes
-app.use('/api/v1/users/', usersRouter)
+app.use('/api/v1/', router);
 
-// testing
-app.get('/', async (req: Request, res: Response) => {
-  await usersService.createUser({
-    id: '9999',
-    password: '1234',
-    role: 'student',
-  })
+// // testing
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   throw new Error('Testing Error logger')
+// })
 
-  res.send('Working successfully')
-})
+// global error handler
+app.use(globalErrorHandler);
 
-export default app
+// handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessage: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
+
+// const testId = async () => {
+//   const testId = await generateFacultyId();
+//   console.log(testId);
+// };
+// testId();
+
+export default app;
---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,66 @@
+import { Schema, model } from 'mongoose';
+import {
+  IAcademicSemester,
+  AcademicSemesterModel,
+} from './academicSemester.interface';
+import {
+  academicSemesterCodes,
+  academicSemesterMonths,
+  academicSemesterTitles,
+} from './academicSemester.constant';
+import ApiError from '../../../errors/ApiError';
+import { StatusCodes } from 'http-status-codes';
+
+const academicSemesterSchema = new Schema<IAcademicSemester>(
+  {
+    title: {
+      type: String,
+      required: true,
+      enum: academicSemesterTitles,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      enum: academicSemesterCodes,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: academicSemesterMonths,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: academicSemesterMonths,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+  if (isExist) {
+    throw new ApiError(
+      StatusCodes.CONFLICT,
+      'Academic semester is already exist !'
+    );
+  }
+  next();
+});
+
+export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
+  'AcademicSemester',
+  academicSemesterSchema
+);
